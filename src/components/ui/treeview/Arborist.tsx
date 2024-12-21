56a3e8f32b398e944ca5d0221186f944de56c932
Jan Peli <janpeli@gmail.com>
2024-12-21 10:37:39 +0100

    tabs and arborist

--- src/components/ui/treeview/Arborist.tsx
+++ src/components/ui/treeview/Arborist.tsx
@@ -1,4 +1,4 @@
-import { Tree, TreeApi } from "react-arborist";
+import { Tree, TreeApi } from "./arborist/index";
 import { useState, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { FilePlus, FolderPlus } from "lucide-react";
--- src/features/Editor/tab.tsx
+++ src/features/Editor/tab.tsx
@@ -7,24 +7,57 @@ import {
   selectOpenFileId,
 } from "@/API/editor-api/editor-api.slice";
 import { closeFile, setActiveFile } from "@/API/editor-api/editor-api";
+import { useCallback, useState } from "react";
 
 export function Tab({ editedFile }: { editedFile: EditedFile }) {
   const openFileID = useAppSelector(selectOpenFileId);
   const dispatch = useAppDispatch();
+  const [dragged, setDragged] = useState(false);
+
+  const dragEndHandler = useCallback(() => {
+    setDragged(false);
+    window.removeEventListener("mouseup", dragEndHandler);
+  }, []);
+
+  const dragStartHandler: React.DragEventHandler<HTMLDivElement> = useCallback(
+    (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      event.dataTransfer.setData("text/plain", editedFile.id);
+      window.addEventListener("mouseup", dragEndHandler);
+      setDragged(true);
+    },
+    [editedFile, dragEndHandler]
+  );
 
   return (
     <div
+      key={editedFile.id}
       className={cn(
         "flex px-2 pt-2 pb-1 items-center gap-1 border-r whitespace-nowrap",
-        editedFile.id == openFileID ? "bg-muted" : ""
+        editedFile.id === openFileID ? "bg-muted" : ""
+        //dragged === true ? "bg-white" : ""
       )}
       onClick={(e) => {
         e.preventDefault();
         setActiveFile(dispatch, editedFile.id);
       }}
+      draggable
+      onDragStart={dragStartHandler}
+      onDragEndCapture={(event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        console.log("drag end captured");
+      }}
+      onDragExitCapture={() => console.log("drag exit captured")}
+      onDragStartCapture={() =>
+        console.log("drag start captured" + editedFile.id)
+      }
+      onDragEnter={() => console.log("drag start captured" + editedFile.id)}
     >
       <File className="w-4 h-4" />
       {editedFile.name}
+      {dragged === true ? "- dragged" : null}
       <Button
         variant="ghost"
         className="w-4 h-4 p-0 hover:bg-muted-foreground"
