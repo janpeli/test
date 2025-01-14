46075d2b2898fa5f7a7ed9d5499ea7b87a23f43d
Jan Peli <janpeli@gmail.com>
2025-01-14 10:43:21 +0100

    cleaned up context bridges

--- electron/src/context-bridges/index.ts
+++ electron/src/context-bridges/index.ts
@@ -1,4 +1,5 @@
-import { ipcRenderer, contextBridge } from "electron";
+import { ipcRenderer, contextBridge, IpcRendererEvent } from "electron";
+import { ProjectStructure } from "../project";
 
 // --------- Expose some API to the Renderer process ---------
 //contextBridge.exposeInMainWorld('ipcRenderer', withPrototype(ipcRenderer))
@@ -20,60 +21,89 @@ import { ipcRenderer, contextBridge } from "electron";
 //  }
 //  return obj
 //}
-// Function to get folder contents
+
 export function setupContextBridges() {
-  const getFolderContents = (folderPath: string) => {
+  // Generic IPC request handler with cleanup and timeout
+  const createIpcRequest = <T, T2>(
+    sendChannel: string,
+    successChannel: string,
+    errorChannel: string,
+    payload?: T2,
+    timeout: number = 30000
+  ): Promise<T> => {
     return new Promise((resolve, reject) => {
-      ipcRenderer.send("get-folder-contents", folderPath);
-      ipcRenderer.once("folder-contents", (_event, files) => {
-        resolve(files);
-      });
-      ipcRenderer.once("folder-contents-error", (_event, error) => {
-        reject(error);
-      });
-    });
-  };
-  // Function to open folder dialog
-  const openFolderDialog = () => {
-    return new Promise((resolve, reject) => {
-      ipcRenderer.send("open-folder-dialog");
-      ipcRenderer.once("folder-selected", (_event, folderPath) => {
-        resolve(folderPath);
-      });
-      ipcRenderer.once("folder-selected-error", (_event, error) => {
-        reject(error);
-      });
-    });
-  };
-
-  const getProjectStructure = (folderPath: string) => {
-    return new Promise((resolve, reject) => {
-      ipcRenderer.send("get-project-contents", folderPath);
-      ipcRenderer.once("project-contents", (_event, data) => {
+      const successHandler = (_event: IpcRendererEvent, data: T) => {
+        cleanup();
         resolve(data);
-      });
-      ipcRenderer.once("folder-contents-error", (_event, error) => {
+      };
+
+      const errorHandler = (_event: IpcRendererEvent, error: string) => {
+        cleanup();
         reject(error);
-      });
+      };
+
+      const cleanup = () => {
+        ipcRenderer.removeListener(successChannel, successHandler);
+        ipcRenderer.removeListener(errorChannel, errorHandler);
+        if (timeoutId) clearTimeout(timeoutId);
+      };
+
+      // Set up listeners
+      ipcRenderer.on(successChannel, successHandler);
+      ipcRenderer.on(errorChannel, errorHandler);
+
+      // Send the request
+      ipcRenderer.send(sendChannel, payload);
+
+      // Set timeout
+      const timeoutId = setTimeout(() => {
+        cleanup();
+        reject(
+          new Error(
+            `IPC request to ${sendChannel} timed out after ${timeout}ms`
+          )
+        );
+      }, timeout);
     });
   };
 
-  const getFileContent = (filePath: string) => {
-    return new Promise((resolve, reject) => {
-      ipcRenderer.send("get-file-contents", filePath);
-      ipcRenderer.once("file-contents", (_event, data) => {
-        resolve(data);
-      });
-      ipcRenderer.once("file-contents-error", (_event, error) => {
-        reject(error);
-      });
-    });
-  };
+  // Specific implementations using the generic handler
+  const getFolderContents = (folderPath: string) =>
+    createIpcRequest<string[], string>(
+      "get-folder-contents",
+      "folder-contents",
+      "folder-contents-error",
+      folderPath
+    );
 
+  const openFolderDialog = () =>
+    createIpcRequest<string, void>(
+      "open-folder-dialog",
+      "folder-selected",
+      "folder-selected-error"
+    );
+
+  const getProjectStructure = (folderPath: string) =>
+    createIpcRequest<ProjectStructure, string>(
+      "get-project-contents",
+      "project-contents",
+      "project-contents-error",
+      folderPath
+    );
+
+  const getFileContent = (filePath: string) =>
+    createIpcRequest<string, string>(
+      "get-file-contents",
+      "file-contents",
+      "file-contents-error",
+      filePath
+    );
+
+  // Expose the methods to the renderer process
   contextBridge.exposeInMainWorld("project", {
-    openFolderDialog: openFolderDialog,
-    getFolderContents: getFolderContents,
-    getProjectStructure: getProjectStructure,
-    getFileContent: getFileContent,
+    openFolderDialog,
+    getFolderContents,
+    getProjectStructure,
+    getFileContent,
   });
 }
