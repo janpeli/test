b0b357ac16a502a5a87149426d1998f2b167f409
Jan Peli <janpeli@gmail.com>
2025-01-23 15:10:36 +0100

    added tooltip to the text area in string field

--- src/features/Editor/editor-from/single-fields/string-field.tsx
+++ src/features/Editor/editor-from/single-fields/string-field.tsx
@@ -10,6 +10,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { FieldProps } from "../editor-single-field";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
+import EditorFormTooltip from "../editor-form-tooltip";
 
 function StringField({ zodKey, schemaField, control }: FieldProps) {
   return (
@@ -20,11 +21,18 @@ function StringField({ zodKey, schemaField, control }: FieldProps) {
       render={({ field }) => (
         <FormItem className={cn(schemaField.format === "text" && "col-span-2")}>
           <FormLabel>
-            {schemaField.title ? schemaField.title : zodKey}
+            <EditorFormTooltip tooltip={schemaField.description || ""}>
+              <span>{schemaField.title ? schemaField.title : zodKey}</span>
+            </EditorFormTooltip>
           </FormLabel>
           <FormControl>
             {schemaField.format === "text" ? (
-              <Textarea placeholder=".." />
+              <Textarea
+                placeholder={
+                  schemaField.description ? schemaField.description : ".."
+                }
+                {...field}
+              />
             ) : (
               <Input placeholder="..." {...field} />
             )}
