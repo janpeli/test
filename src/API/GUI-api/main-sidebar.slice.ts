8ac719767c7fa34b06561256a10e7cb967b51490
janpeli <janpeli@gmail.com>
2025-03-21 07:31:47 +0100

    prep for active context

--- src/API/GUI-api/main-sidebar.slice.ts
+++ src/API/GUI-api/main-sidebar.slice.ts
@@ -1,31 +1,37 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import type { RootState } from '../../app/store'
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../../app/store";
 
 // Define a type for the slice state
 interface MainSidebarState {
-  activeMenu: string
+  activeMenu: string;
+  selectedProjectItem: string;
 }
 
 // Define the initial state using that type
 const initialState: MainSidebarState = {
-    activeMenu: "off"
-}
+  activeMenu: "off",
+  selectedProjectItem: "",
+};
 
 export const mainSidebarSlice = createSlice({
-  name: 'mainSidebar',
+  name: "mainSidebar",
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
     // Use the PayloadAction type to declare the contents of `action.payload`
     setActiveMenu: (state, action: PayloadAction<string>) => {
-      state.activeMenu = action.payload
-    }
+      state.activeMenu = action.payload;
+    },
+    selectedProjectItem: (state, action: PayloadAction<string>) => {
+      state.selectedProjectItem = action.payload;
+    },
+  },
+});
 
-}})
-
-export const {  setActiveMenu } = mainSidebarSlice.actions
+export const { setActiveMenu } = mainSidebarSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectActiveMenu = (state: RootState) => state.mainSidebar.activeMenu
+export const selectActiveMenu = (state: RootState) =>
+  state.mainSidebar.activeMenu;
 
-export default mainSidebarSlice.reducer
+export default mainSidebarSlice.reducer;
